0291d2a57131c3d3029360959555f89a275097e9
jkennan <jkennan97@gmail.com>
2017-07-22 12:58:06 -0400

    Adds basic timer functionality to popup page

--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,55 @@
 let microInterval = 600000; // Hard-coded @ 10 mins
-let restInterval  = 1800000; // Hard-set @ 30 mins
-let reps          = 0;
-let totalTime     = 0;
+let restInterval = 1800000; // Hard-set @ 30 mins
+let reps = 3;
+let totalTime = 0;
+let timerVar;
+
+
+// Startups
+console.log(70 / 60 % 60);
+
 
 function startClock() {
 
-    setInterval(function() {
-       totalTime++;
+    timerVar = setInterval(function () {
+        totalTime++;
+
+        if (totalTime == 600) {
+            checkReps();
+        }
+        reloadPage();
     }, 1000);
 }
 
 
-function checkReps() {
-    if (reps != 3) {
-        reps++;
-    } else {
+function stopClock() {
+    clearInterval(timerVar);
+}
 
+
+function checkReps() {
+    if (reps != 0) {
+        reps--;
+    } else {
+        alert("else condition reached");
     }
 }
 
 
-function reloadPage() {
-    document.getElementById("micro-break-time-left").innerHTML = getTimeString(); 
+function reloadPage_dep() {
+    document.getElementById("micro-break-time-left").innerHTML = getTimeString();
 }
 
-function getTimeString(time) {
-    let min = (time / 60) % 60;
-    return min + ":" + totalTime;
+function reloadPage() {
+
+    // Create time objects
+    let secs = totalTime % 60;
+    let mins = Math.floor(totalTime / 60);
+    // Hours code to be implemented later
+    // let hrs = Math.floor(mins / 60);
+
+    // Create and update popup page
+    document.getElementById("total-time-elapsed").innerHTML = String(mins) + ":" + (secs < 10 ? "0" + String(secs) : String(secs));
+    document.getElementById("micro-break-time-left").innerHTML = String(9 - mins) + ":" + (String(60 - secs) < 10 ? "0" + String(60 - secs) : String(60 - secs));
+    document.getElementById("rest-break-time-left").innerHTML = String((10 * reps - 1) - mins) + ":" + (String(60 - secs) < 10 ? "0" + String(60 - secs) : String(60 - secs));
 }
